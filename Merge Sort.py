2e21f7a3d1ad985606eb2da310880f56b245f15d
JoshuaMina <joshua.minacambronero@gmail.com>
2023-01-26 00:43:48 +0800

    Merge

--- Merge Sort.py
+++ Merge Sort.py
@@ -16,4 +16,7 @@ def merge(arr):
             if left_arr[i] < right_arr[j]:
                 arr[k] = left_arr[i]
                 i += 1
-
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
