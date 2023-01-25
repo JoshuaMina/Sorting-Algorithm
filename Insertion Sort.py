2f2ff05901a10b2e158ecfe65d38bb2757b08d76
JoshuaMina <joshua.minacambronero@gmail.com>
2023-01-26 02:14:49 +0800

    Insertion

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -3,6 +3,12 @@ print("\t The Given Unsorted Array is:", nums,"\n")
 
 def insert(arr):
     for i in range(1, len(arr)):
-        j = 1
+        j = i
         while arr[j - 1] > arr[j] and j > 0:
             arr[j-1], arr[j] = arr[j], arr[j-1]
+            j -= 1
+        print("\t\t", arr)
+    return arr
+insert(nums)
+
+print("\t\n The Final Sorted Array is:",insert(nums))
