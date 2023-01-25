082ce7db2f4d9f970545fcb31cec7803db315c1c
JoshuaMina <joshua.minacambronero@gmail.com>
2023-01-26 00:23:59 +0800

    Bubble

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,9 +1,14 @@
 nums = [64, 97, 76, 20, 29, 34, 60, 89, 57, 85]
 print("\t The Given Unsorted Array is:", nums,"\n")
 
-for i in range(len(nums)-1,0,-1):
-    for j in range(i):
-        if nums[j] > nums[j + 1]:
-            temp = nums[j]
-            nums[j] = nums[j + 1]
-            nums[j + 1] = temp
+def bubble(nums):
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j] > nums[j + 1]:
+                temp = nums[j]
+                nums[j] = nums[j + 1]
+                nums[j + 1] = temp
+
+bubble(nums)
+
+print("\t\n The Final Sorted Array is:",nums)
